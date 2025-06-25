1c6a0ef3ebee111697b06acb3923b077c3562aa7
Ram <sairamdanda123@gmail.com>
2025-06-25 08:50:54 +0530

    Refactor DataCollectionPanel: Update ToggleSwitch behavior to manage edit mode state and remove unused edit button and instructions for cleaner UI.

--- darsa/src/components/DataCollectionPanel.jsx
+++ darsa/src/components/DataCollectionPanel.jsx
@@ -31,7 +31,18 @@ const DataCollectionPanel = ({
                     <label className="control-label">Edit ROI</label>
                     <ToggleSwitch
                         isChecked={editROI}
-                        onChange={() => setEditROI(!editROI)}
+                        onChange={() => {
+                            setEditROI(!editROI);
+                            if(editROI){
+                                setIsEditMode(false);
+                            }else{
+                                setIsEditMode(true);
+                            }
+                            setIsRemoveMode(false);
+                            setIsAddMode(false);
+                        }
+
+                        }
                     />
                 </div>
             </div>
@@ -130,23 +141,6 @@ const DataCollectionPanel = ({
                             Add
                         </button>
                         <button
-                            className={`tool-btn ${isEditMode ? 'active' : ''}`}
-                            onClick={() => {
-                                setIsEditMode(!isEditMode);
-                                if (!isEditMode) {
-                                    setIsAddMode(false);
-                                    setIsRemoveMode(false);
-                                }
-                            }}
-                            title="Edit existing polygons"
-                        >
-                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                                <path d="m18.5 2.5 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-                            </svg>
-                            Edit
-                        </button>
-                        <button
                             className={`tool-btn ${isRemoveMode ? 'active' : ''}`}
                             onClick={() => {
                                 setIsRemoveMode(!isRemoveMode);
@@ -178,41 +172,6 @@ const DataCollectionPanel = ({
                             <line x1="5" y1="12" x2="19" y2="12"/>
                         </svg>
                     </button>
-                </div>
-            </div>
-
-            {/* Instructions */}
-            <div className="instructions-panel">
-                <h4 className="instructions-title">Instructions</h4>
-                <div className="instructions-content">
-                    {isAddMode && (
-                        <ul className="instruction-list">
-                            <li>Click on the image to add points for new polygon</li>
-                            <li>Click on existing polygon edges to add points to them</li>
-                            <li>Create at least 3 points to form a polygon</li>
-                            <li>Press <kbd>Enter</kbd> to complete polygon</li>
-                            <li>Press <kbd>Esc</kbd> to cancel current polygon</li>
-                        </ul>
-                    )}
-                    {isEditMode && (
-                        <ul className="instruction-list">
-                            <li>Click and drag points to move them</li>
-                            <li>Click and drag polygons to move entire zones</li>
-                            <li>Press <kbd>Delete</kbd> to remove selected polygon</li>
-                        </ul>
-                    )}
-                    {isRemoveMode && (
-                        <ul className="instruction-list">
-                            <li>Click on points to remove them</li>
-                            <li>Click on polygons to delete entire zones</li>
-                            <li>Polygons with less than 3 points are automatically removed</li>
-                        </ul>
-                    )}
-                    {!isAddMode && !isEditMode && !isRemoveMode && (
-                        <p className="instruction-text">
-                            Select a tool above to start editing collection zones.
-                        </p>
-                    )}
                 </div>
             </div>
         </div>
